45b4c057655ef22aced225447c975b58305a1537
Daniel Earwicker <dan@earwicker.com>
2013-07-21 20:48:30 +0100

    Separated scrollbars and mousewheel support, supports unlimited datasets (e.g. a billion rows!) but behaves a little weirdly on Mac OS

--- main.js
+++ main.js
@@ -4,10 +4,11 @@
 window.onload = function() {
 
     var columns = [
-        { label: 'ID', template: '<span data-bind="text: id"></span>', width: 60 },
+        { label: 'ID', template: '<span data-bind="text: id"></span>', width: 70 },
         { label: 'First name', template: '<span data-bind="text: firstName"></span>', width: 120 },
         { label: 'Middle name', template: '<span data-bind="text: middleName"></span>', width: 120 },
-        { label: 'Last name', template: '<span data-bind="text: lastName"></span>', width: 160 }
+        { label: 'Last name', template: '<span data-bind="text: lastName"></span>', width: 160 },
+        { label: 'Full name', template: '<span data-bind="text: firstName + \' \' + middleName + \' \' + lastName"></span>', width: 220 }
     ];
 
     var firstNames = ['Homer', 'Marge', 'Bart', 'Lisa', 'Maggie', 'Moe', 'Barney', 'Carl', 'Lenny', 'Ned', 'Milhouse'];
@@ -23,58 +24,85 @@ window.onload = function() {
     };
 
     var rows = [];
-    for (var r = 123; r<223; r++) {
+    for (var r = 0; r<100; r++) {
         rows.push(makeRow(r));
     }
 
     var viewModel = {
         columns: columns,
-        rows: ko.observable(rows),
 
-        rowOffset: ko.observable(123),
-        rowCount: ko.observable(100000),
+        rowOffset: ko.observable(0),
+        rowCount: ko.observable(1000000000),
 
         rowHeight: ko.observable(24),
+        visibleRowsHeight: ko.observable(0),
 
         scrollLeft: ko.observable(0),
-        scrollTop: ko.observable(0),
-        gridHeight: ko.observable(0)
+
+        vViewport: ko.observable(0),
+        hViewport: ko.observable(0)
     };
 
-    viewModel.paddingTop = ko.computed(function() {
-        return viewModel.rowOffset() * viewModel.rowHeight();
+    viewModel.visibleRows = ko.computed(function() {
+        return Math.ceil(viewModel.visibleRowsHeight() / viewModel.rowHeight());
     });
 
-    viewModel.paddingBottom = ko.computed(function() {
-        return Math.max(0, (viewModel.rowCount() -
-            (viewModel.rowOffset() + viewModel.rows().length))
-                * viewModel.rowHeight());
+    viewModel.verticalRange = ko.computed(function() {
+        return viewModel.rowCount() - viewModel.visibleRows();
     });
 
-    var cachedFirst = -1, cachedLast = -1;
+    viewModel.vPadding = ko.computed(function() {
+        return Math.min(100000, Math.max(viewModel.vViewport(),
+            viewModel.rowHeight() * viewModel.verticalRange()));
+    });
 
-    ko.computed(function() {
-        var first = Math.floor(viewModel.scrollTop() / viewModel.rowHeight());
-        var last = Math.ceil((viewModel.scrollTop() + viewModel.gridHeight()) / viewModel.rowHeight());
-
-        var pageSize = 50;
-
-        first = Math.max(0, Math.floor(first / pageSize) * pageSize);
-        last = Math.min(Math.ceil(last / pageSize) * pageSize, viewModel.rowCount());
-
-        if (first != cachedFirst || last != cachedLast) {
-            cachedFirst = first;
-            cachedLast = last;
-
-            var rows = [];
-            for (var r = first; r < last; r++) {
-                rows.push(makeRow(r));
-            }
-
-            viewModel.rowOffset(first);
-            viewModel.rows(rows);
+    viewModel.vScroll = ko.computed({
+        read: function() {
+            var proportion = viewModel.rowOffset() / viewModel.verticalRange();
+            return Math.round(proportion * (viewModel.vPadding() - viewModel.vViewport()));
+        }, write: function(v) {
+            var proportion = v / (viewModel.vPadding() - viewModel.vViewport());
+            viewModel.rowOffset(Math.round(viewModel.verticalRange() * proportion));
         }
     });
+
+    viewModel.allColumnsWidth = ko.computed(function() {
+        return viewModel.columns.map(function(c) {
+            return ko.unwrap(c.width);
+        }).reduce(function(l, r) {
+            return l + r;
+        });
+    });
+
+    viewModel.horizontalRange = ko.computed(function() {
+        return Math.max(0, viewModel.allColumnsWidth() - viewModel.hViewport());
+    });
+
+    viewModel.hPadding = ko.computed(function() {
+        return viewModel.hViewport() + viewModel.horizontalRange();
+    });
+
+    viewModel.hScroll = ko.computed({
+        read: function() {
+            var proportion = viewModel.scrollLeft() / viewModel.horizontalRange();
+            return Math.round(proportion * (viewModel.hPadding() - viewModel.hViewport()));
+        }, write: function(v) {
+            var proportion = v / (viewModel.hPadding() - viewModel.hViewport());
+            viewModel.scrollLeft(Math.round(viewModel.horizontalRange() * proportion));
+        }
+    });
+
+    viewModel.rows = ko.computed(function() {
+
+        var rows = [];
+        var end = viewModel.rowOffset() + viewModel.visibleRows();
+        for (var r = viewModel.rowOffset(); r < end; r++) {
+            rows.push(makeRow(r));
+        }
+
+        return rows;
+
+    }).extend({ throttle: 10});
 
     ko.applyBindings(viewModel);
 };
