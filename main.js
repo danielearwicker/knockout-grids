a032064a0df0be57fc114424d8e503491ceaa572
Daniel Earwicker <dan@earwicker.com>
2013-07-21 09:38:03 +0100

    Virtualising via top/bottom padding divs

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ window.onload = function() {
     };
 
     var rows = [];
-    for (var r = 0; r<1000; r++) {
+    for (var r = 123; r<223; r++) {
         rows.push(makeRow(r));
     }
 
@@ -31,10 +31,50 @@ window.onload = function() {
         columns: columns,
         rows: ko.observable(rows),
 
+        rowOffset: ko.observable(123),
+        rowCount: ko.observable(100000),
+
         rowHeight: ko.observable(24),
 
-        scrollLeft: ko.observable(0)
+        scrollLeft: ko.observable(0),
+        scrollTop: ko.observable(0),
+        gridHeight: ko.observable(0)
     };
+
+    viewModel.paddingTop = ko.computed(function() {
+        return viewModel.rowOffset() * viewModel.rowHeight();
+    });
+
+    viewModel.paddingBottom = ko.computed(function() {
+        return Math.max(0, (viewModel.rowCount() -
+            (viewModel.rowOffset() + viewModel.rows().length))
+                * viewModel.rowHeight());
+    });
+
+    var cachedFirst = -1, cachedLast = -1;
+
+    ko.computed(function() {
+        var first = Math.floor(viewModel.scrollTop() / viewModel.rowHeight());
+        var last = Math.ceil((viewModel.scrollTop() + viewModel.gridHeight()) / viewModel.rowHeight());
+
+        var pageSize = 50;
+
+        first = Math.max(0, Math.floor(first / pageSize) * pageSize);
+        last = Math.min(Math.ceil(last / pageSize) * pageSize, viewModel.rowCount());
+
+        if (first != cachedFirst || last != cachedLast) {
+            cachedFirst = first;
+            cachedLast = last;
+
+            var rows = [];
+            for (var r = first; r < last; r++) {
+                rows.push(makeRow(r));
+            }
+
+            viewModel.rowOffset(first);
+            viewModel.rows(rows);
+        }
+    });
 
     ko.applyBindings(viewModel);
 };
